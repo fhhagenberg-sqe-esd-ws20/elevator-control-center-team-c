729e96443acc003b8675485a0ef100b2e99e6d3d
Stefan Wohlrab <stefan.wohlrab@students.fh-hagenberg.at>
2020-12-17 10:51:45 +0100

    Add structure for ElevatorMock

---
+++ src/main/java/at/fhhagenberg/sqe/esd/ws20/model/IElevatorWrapper.java
@@ -0,0 +1,17 @@
+package at.fhhagenberg.sqe.esd.ws20.model;
+
+public interface IElevatorWrapper {
+
+	
+	
+	
+	
+	
+	
+	/**
+	 * Retrieves the current clock tick of the elevator control system. 
+	 * @return clock tick
+	 */
+	public long getClockTick() throws java.rmi.RemoteException;
+	
+}
---
+++ src/test/java/at/fhhagenberg/sqe/esd/ws20/model/ElevatorSimulatorMock.java
@@ -0,0 +1,15 @@
+package at.fhhagenberg.sqe.esd.ws20.model;
+
+public class ElevatorSimulatorMock implements IElevatorWrapper 
+{
+
+	
+	
+	
+	
+
+	public long getClockTick() throws java.rmi.RemoteException{
+		return 1;
+	}
+	
+}
