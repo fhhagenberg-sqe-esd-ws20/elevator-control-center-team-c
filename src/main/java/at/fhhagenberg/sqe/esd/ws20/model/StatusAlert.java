672a4c9475a0c0745f9b427332996f810db5970e
Steckdoose4711 <atzenhoferflorian@gmail.com>
2020-12-28 16:15:19 +0100

    Created StatusAlert

---
+++ src/main/java/at/fhhagenberg/sqe/esd/ws20/model/StatusAlert.java
@@ -0,0 +1,8 @@
+package at.fhhagenberg.sqe.esd.ws20.model;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class StatusAlert {
+	public StringProperty Status = new SimpleStringProperty();
+}
