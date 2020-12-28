0a4a8ab87e3f6684a95d0cf1cc6aa37a2d1d7f0a
Steckdoose4711 <atzenhoferflorian@gmail.com>
2020-12-28 15:36:52 +0100

    added getter and setter for BuildingModel

--- src/main/java/at/fhhagenberg/sqe/esd/ws20/model/BuildingModel.java
+++ src/main/java/at/fhhagenberg/sqe/esd/ws20/model/BuildingModel.java
@@ -1,8 +1,42 @@
 package at.fhhagenberg.sqe.esd.ws20.model;
 
+import java.util.List;
 
 public class BuildingModel implements IBuildingModel {
 
 	
-	 private int x;
+	public void SetNumElevators(int numElevators)
+	{
+		NumElevators = numElevators;
+	}
+	
+	public void SetNumFloors(int numFloors)
+	{
+		NumFloors = numFloors;
+	}
+	
+	public void SetServiceFloors(List<Integer> serviceFloors)
+	{
+		ServiceFloors = serviceFloors;
+	}	
+	
+	public int GetNumElevators()
+	{
+		return NumElevators;
+	}
+	
+	public int GetNumFloors()
+	{
+		return NumFloors;
+	}
+	
+	public List<Integer> GetServiceFloors()
+	{
+		return ServiceFloors;
+	}	
+	
+	 private int NumElevators;
+	 private int NumFloors;
+	 private List<Integer> ServiceFloors;
+
 }
---
+++ src/main/java/at/fhhagenberg/sqe/esd/ws20/model/ElevatorModel.java
@@ -0,0 +1,5 @@
+package at.fhhagenberg.sqe.esd.ws20.model;
+
+public class ElevatorModel {
+
+}
