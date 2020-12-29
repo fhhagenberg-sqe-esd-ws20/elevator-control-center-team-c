869b1090e939bd3b172e1a53b4bcca354028e7af
Steckdoose4711 <atzenhoferflorian@gmail.com>
2020-12-29 11:05:50 +0100

    changed interface of the controllers "Update" Methode added functionality in UpdateData

--- src/main/java/at/fhhagenberg/sqe/esd/ws20/model/UpdateData.java
+++ src/main/java/at/fhhagenberg/sqe/esd/ws20/model/UpdateData.java
@@ -23,9 +23,8 @@ public class UpdateData extends TimerTask {
 		Sqelevator = sqelevator;
 		GuiController = guiController;
 		
-		initializeBuilding();
-
-		
+		//TODO: Activate this line, when mock is used for the elevatorinterface
+		//initializeBuilding();
 	}
 	
 
@@ -62,28 +61,6 @@ public class UpdateData extends TimerTask {
         try {
         	System.out.println("Getting Data from Simulator");
         	
-        	//get current clocktick to guarantee atomar access
-        	long clocktickBeforeUpdate = Sqelevator.getClockTick();
-        	
-        	//
-        	//TODO: Alle Lifte hier updaten? Oder jeden Lift einzeln? Es gibt nur eine einzige Updatemethode
-        	// Get parameters for all elevators
-        	//.
-        	//.
-        	//.
-        	// ready with reading params for the elevator
-        	
-        	
-        	// check, if clocktick of the sqelevator has changed in the meantime
-        	if(Sqelevator.getClockTick() != clocktickBeforeUpdate)
-        	{
-        		System.out.println("out of sync with the simulator");
-        	}
-        	else
-        	{
-        		// everything is okay. Notify the Controller, that the elevator has been updated
-        		GuiController.update();
-        	}
 
         	
         } catch (Exception ex) {
@@ -95,13 +72,61 @@ public class UpdateData extends TimerTask {
      */
     public void setSelectedElevator(int elevatorIdx)
     {
-    	if(elevatorIdx > 0 && elevatorIdx < Elevators.size())
+    	if(elevatorIdx >= 0 && elevatorIdx < Elevators.size())
 		{
 			SelectedElevator = elevatorIdx;
 		}
     }
     
+    /**
+     * Stop was pressed in an elevator. The Target will be added to the targetlist of the elevator
+     */
+    public void setTarget(int floor, int elevator)
+    {
+    	if(floor >= 0 && floor <= Building.GetNumFloors())
+		{
+			if(elevator >= 0 && elevator < Elevators.size())
+			{
+				IElevatorModel el = Elevators.get(elevator);
+				el.AddStop(floor);
+			}
+		}
+    }
     
+    
+    //TODO: Was soll diese Funktion machen?
+    /**
+     * 
+     */
+    public void setTarget(int floor)
+    {
+
+    }
+    
+    
+    /**
+     * Refresh list with pressed up and down buttons and notify the gui to show them
+     */
+    public void refreshUpDownList() throws RemoteException
+    {
+    	//get current clocktick to guarantee atomar access
+    	long clocktickBeforeUpdate = Sqelevator.getClockTick();
+    	
+    	
+    	
+    	
+    	
+    	// check, if clocktick of the sqelevator has changed in the meantime
+    	if(Sqelevator.getClockTick() != clocktickBeforeUpdate)
+    	{
+    		System.out.println("out of sync with the simulator");
+    	}
+    	else
+    	{
+    		// everything is okay. Notify the Controller, that updownlist has been updated
+    		GuiController.update(Building, Floor, Elevators.get(SelectedElevator));
+    	}
+    }
     
 	private IElevatorWrapper Sqelevator;
 	private IBuildingWrapper SqBuilding;
