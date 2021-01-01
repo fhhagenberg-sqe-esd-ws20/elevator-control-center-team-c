2331ecdb4a53df7cea8c91109f5b8fbc242635f3
Stefan Wohlrab <stefan.wohlrab@students.fh-hagenberg.at>
2021-01-01 21:06:58 +0100

    Add BuildingModel unit tests

---
+++ src/test/java/at/fhhagenberg/sqe/esd/ws20/model/BuildingModelTest.java
@@ -0,0 +1,39 @@
+package at.fhhagenberg.sqe.esd.ws20.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+
+public class BuildingModelTest {
+
+    @Test
+    void testCreate_and_get() {
+    	BuildingModel buildingModel = new BuildingModel();
+    	
+        assertEquals(0, buildingModel.getNumElevators());
+        assertEquals(0, buildingModel.getNumFloors());
+    }
+    
+    @Test
+    void testNumElevaors_Set_and_get() {
+    	BuildingModel buildingModel = new BuildingModel();
+    	
+    	buildingModel.setNumElevators(1);
+    	
+        assertEquals(1, buildingModel.getNumElevators());
+    }
+    
+    @Test
+    void testNumFloors_Set_and_get() {
+    	BuildingModel buildingModel = new BuildingModel();
+    	
+    	buildingModel.setNumFloors(2);
+    	
+        assertEquals(2, buildingModel.getNumFloors());
+    }
+	
+}
