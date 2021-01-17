b35b479f144ce0d88c40f73903ad5e94f4708087
Stefan Wohlrab <stefan.wohlrab@students.fh-hagenberg.at>
2021-01-17 17:56:40 +0100

    Move generateNotServicedFloors to RMIConnection

--- src/main/java/at/fhhagenberg/sqe/esd/ws20/model/UpdateData.java
+++ src/main/java/at/fhhagenberg/sqe/esd/ws20/model/UpdateData.java
@@ -74,46 +74,7 @@ public class UpdateData extends TimerTask {
 	}
 	
 	
-	/**
-	 * generate floors that are not serviced. This is not done by the simulator -> we have to do it.
-	 * If there are already not serviced floors set, this function does not overwrite them and returns.
-	 */
-	private void generateNotServicedFloors() {
-		//ground floor gets always serviced according to specification
-		if (buildingModel.getNumFloors() <= 1) {
-			return;
-		}
-		
-		int numElevators = buildingModel.getNumElevators();
-		int numFloors = buildingModel.getNumFloors();
-		
-		//check if there are already no serviced floors set -> don't overwrite them
-		boolean anyFloorNotServiced = false;
-		for(int elevator = 0; elevator < numElevators; elevator++) {
-			for(int floor = 0; floor < numFloors; floor++) {
-				try {
-					if(!sqElevator.getServicesFloors(elevator, floor)) anyFloorNotServiced = true;
-				} catch (RemoteException e) {
-					statusAlertContext.setStatus("Exception in generateNotServicedFloors() 1 of SQElevator with floor " + floor + " and elevator  " + elevator);
-				}
-			}
-		}
-		if(anyFloorNotServiced) {
-			return;
-		}
-		
-		//all floors are serviced -> add some not serviced ones
-		SecureRandom random = new SecureRandom();
-		for(int i = 0; i < NOT_SERVICED_FLOORS_RANDOM_ITERATIONS; i++) {
-			int elevator = random.nextInt(numElevators);
-			int floor = random.nextInt(numFloors);	//this can also generate 0. This will be ignored by elevator simulator as ground floor will always be serviced.
-			try {
-				sqElevator.setServicesFloors(elevator, floor, false);
-			} catch (RemoteException e) {
-				statusAlertContext.setStatus("Exception in generateNotServicedFloors() 2 of SQElevator with floor " + floor + " and elevator  " + elevator);
-			}
-		}
-	}
+
 	
 	
 	/** 
@@ -128,9 +89,6 @@ public class UpdateData extends TimerTask {
 		{
 			throw new InvalidParameterException("Numer of stored elevators not the same as number of elevators in the building!");
 		}
-		
-		// generate floors that are not serviced. This is not done by the simulator -> we have to do it
-		generateNotServicedFloors();
 		
 		// get all servicefloors of each elevator and store them in a list
 		for(int elevator = 0; elevator < elevatorsList.size(); elevator++)
@@ -696,5 +654,4 @@ public class UpdateData extends TimerTask {
     private AutoMode autoModeAlgorithmContext;
     private IRMIConnection rmiConnectionContext;
     private long lastTick = 0;
-    private static final int NOT_SERVICED_FLOORS_RANDOM_ITERATIONS = 15;
 }
--- src/main/java/at/fhhagenberg/sqe/esd/ws20/sqeelevator/RMIConnection.java
+++ src/main/java/at/fhhagenberg/sqe/esd/ws20/sqeelevator/RMIConnection.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.security.SecureRandom;
 
 import sqelevator.IElevator;
 
@@ -14,10 +15,48 @@ public class RMIConnection implements IRMIConnection {
 	    IElevator elevator = null;
 		try {
 			elevator = (IElevator) Naming.lookup("rmi://localhost/ElevatorSim");
+			//not done by simulator -> generate random floors which are not serviced to show functionality
+			generateNotServicedFloors(elevator);
 		} catch (MalformedURLException | RemoteException | NotBoundException e) {
 			//Nothing to do, just return null ptr
 		}
 		return elevator;
 	}
+	
+	
+	/**
+	 * generate floors that are not serviced. This is not done by the simulator -> we have to do it.
+	 * If there are already not serviced floors set, this function does not overwrite them and returns.
+	 */
+	private void generateNotServicedFloors(IElevator elevator) throws RemoteException {
+		//ground floor gets always serviced according to specification
+		if (elevator.getFloorNum() <= 1) {
+			return;
+		}
+		
+		int numElevators = elevator.getElevatorNum();
+		int numFloors = elevator.getFloorNum();
+		
+		//check if there are already no serviced floors set -> don't overwrite them
+		boolean anyFloorNotServiced = false;
+		for(int elev = 0; elev < numElevators; elev++) {
+			for(int floor = 0; floor < numFloors; floor++) {
+				if(!elevator.getServicesFloors(elev, floor)) anyFloorNotServiced = true;
+			}
+		}
+		if(anyFloorNotServiced) {
+			return;
+		}
+		
+		//all floors are serviced -> add some not serviced ones
+		SecureRandom random = new SecureRandom();
+		for(int i = 0; i < NOT_SERVICED_FLOORS_RANDOM_ITERATIONS; i++) {
+			int elev = random.nextInt(numElevators);
+			int floor = random.nextInt(numFloors);	//this can also generate 0. This will be ignored by elevator simulator as ground floor will always be serviced.
+				elevator.setServicesFloors(elev, floor, false);
+		}
+	}
+	
+    private static final int NOT_SERVICED_FLOORS_RANDOM_ITERATIONS = 15;
 
 }
